ea08b432970cf4a4daf36a330076e2da89b74c68
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2021-05-13 16:56:00 +0500

    Initial commit

---
+++ src/Die.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Die {
+    int value;
+
+    public Die() {
+        this.value =0;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int roll() { return 1 + (int) (Math.random() * 6);}
+
+    public boolean equal(Die a,Die b){
+        return a.equals(b)?true:false;
+    }
+
+    @Override
+    public String toString() {
+        switch (value){
+            case 1:
+                return "One" ;
+            case 2:
+                return "Two";
+            case 3:
+                return "Three";
+            case 4:
+                return "Four";
+            case 5:
+                return "Five";
+            case 6:
+                return "Six";
+        }
+        return null;
+    }
+}
---
+++ src/DoubleDice.java
@@ -0,0 +1,51 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class DoubleDice {
+    private static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        float amount = 0.0F,bet = 0.0F;
+        do {
+            System.out.println("Enter the Amount :");
+             amount = input.nextFloat();
+            if (amount!=0.00){
+                break;
+            }
+        }while (true);
+
+
+        do {
+            if (bet>amount ){
+                System.out.println("\n\nYou are Out of Money!\nBetter Luck Next Time !");
+                return;
+            }
+
+            DecimalFormat decimalFormat = new DecimalFormat("#.##");
+            float twoDigitsF = Float.valueOf(decimalFormat.format(amount));
+            System.out.println("\n\nYou Have $"+twoDigitsF);
+            System.out.println("\nHow Much would you Like to bet (Enter 0 to quit )? ");
+            bet = input.nextFloat();
+            if (bet ==0){
+                System.out.println("See you Around, winner!");
+                return;
+            }
+            if (bet>amount ){
+                System.out.println("\n\nYou are Out of Money!\nBetter Luck Next Time !");
+                return;
+            }
+
+            Die die = new Die();
+            Die die1 = new Die();
+            die.setValue(die.roll());
+            die1.setValue(die1.roll());
+            System.out.println("You Rolled a "+ die +" and "+die1);
+            if(die.equal(die,die1)){
+                System.out.println("You win $"+bet);
+                amount = amount+bet;
+            }else {
+                System.out.println("You Loss $"+bet);
+                amount = amount-bet;
+            }
+        }while (true);
+    }
+}
